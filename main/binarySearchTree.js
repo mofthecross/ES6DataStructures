6ca3f5ceede1590ace2c0dedf03108d27bd08657
mofthecross <mofthecross@gmail.com>
2016-12-30 20:28:00 -0800

    Implement insert method on BinarySearchTree.js

---
+++ main/binarySearchTree.js
@@ -0,0 +1,41 @@
+class Node {
+	constructor(value) {
+		this.value = !value ? null : value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BinarySearchTree {
+	constructor(){
+		this.root = null;
+    this.size = 0;
+	}
+  insert(value) {
+    if(!this.root) {
+      this.root = new Node(value);
+      this.size++;
+    } else {
+      let currentNode = this.root;
+      while(currentNode.value) {
+        if (value < currentNode.value) {
+          if (currentNode.left === null) {
+            currentNode.left = new Node(value);
+            this.size++;
+            break;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else {
+          if (currentNode.right === null) {
+            currentNode.right = new Node(value);
+            this.size++;
+            break;
+          } else {
+            currentNode = currentNode.right;
+          }
+        }
+      }
+    }
+  }
+}
