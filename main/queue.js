24217b1f0cfba4788ba5f2da6f47a24829fbce39
mofthecross <mofthecross@gmail.com>
2017-01-03 23:25:18 -0800

    Implement queue data structure

--- main/queue.js
+++ main/queue.js
@@ -4,15 +4,17 @@ class Queue {
     this._newestIndex = 1;
     this._storage = {};
   }
+
   enqueue(value) {
     this._storage[this._newestIndex] = value;
     this._newestIndex++;
   }
+  
   dequeue(value) {
     if (this._oldestIndex !== this._newestIndex) {
       const dequeued = this._storage[this._oldestIndex];
       delete this._storage[this._oldestIndex];
-      this._oldestIndex = this._oldestIndex++;
+      this._oldestIndex++
       return dequeued
     }
   }
