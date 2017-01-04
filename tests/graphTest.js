eefce72b977d4ef40629dae68f165a0dd9f6993f
mofthecross <mofthecross@gmail.com>
2017-01-03 19:54:30 -0800

    i Please enter the commit message for your changes. Lines starting

--- tests/graphTest.js
+++ tests/graphTest.js
@@ -33,178 +33,43 @@ describe('graph class', function(){
       expect(graph).to.respondTo('removeEdge');
       expect(graph).to.respondTo('findNeighbors');
       expect(graph).to.respondTo('forEachNode');
+    });
+  });
+
+  describe('graph addVertex method', function(){
+    it('should add a single vertex', function(){
+      const graph = new test.Graph();
+      expect(graph.vertices['mofthecross']).to.equal(undefined);
+
+      graph.addVertex('mofthecross');
+      expect(graph.vertices['mofthecross'].value).to.equal('mofthecross');
+      expect(graph.totalVertices).to.equal(1);
     })
-  })
-  //
-  // describe('graph addVertex method', function(){
-  //   it('should add a single vertex', function(){
-  //     const test = new Graph();
-  //     expect(test.vertices['hello']).to.equal(undefined);
-  //
-  //     test.addVertex('hello');
-  //     expect(test.vertices['hello'].value).to.equal('hello');
-  //     expect(test.totalVertices).to.equal(1);
-  //   })
-  //
-  //   it('should add two vertices', function(){
-  //     const test = new Graph();
-  //     expect(test.vertices['hello']).to.equal(undefined);
-  //
-  //     test.addVertex('hello');
-  //     expect(test.vertices['hello'].value).to.equal('hello');
-  //     expect(test.totalVertices).to.equal(1);
-  //
-  //     test.addVertex('good');
-  //     expect(test.vertices['good'].value).to.equal('good');
-  //     expect(test.totalVertices).to.equal(2);
-  //   })
-  //
-  //   it('should not increase the totalVertices count when the vertex already exists', function(){
-  //     const test = new Graph();
-  //     expect(test.vertices['hello']).to.equal(undefined);
-  //
-  //     test.addVertex('hello');
-  //     expect(test.vertices['hello'].value).to.equal('hello');
-  //     expect(test.totalVertices).to.equal(1);
-  //
-  //     test.addVertex('hello');
-  //     expect(test.totalVertices).to.equal(1);
-  //   })
-  // })
-  //
-  // describe('graph getVertex method', function(){
-  //   it('should return the entire vertex of the queried ID', function(){
-  //     const test = new Graph();
-  //
-  //     test.addVertex('hello');
-  //     const result = test.getVertex('hello');
-  //
-  //     expect(result.value).to.equal('hello');
-  //     expect(result).to.have.property('value');
-  //     expect(result).to.have.property('edges');
-  //   })
-  //
-  //   it('should return undefined when the vertex ID doesn not exist', function(){
-  //     const test = new Graph();
-  //
-  //     const result = test.getVertex('notHere');
-  //
-  //     expect(result).to.equal(undefined);
-  //   })
-  // })
-  //
-  // describe('graph removeVertex method', function(){
-  //   it('should remove a vertex when deleted', function(){
-  //     const test = new Graph();
-  //
-  //     test.addVertex('hello');
-  //     expect(test.vertices['hello'].value).to.equal('hello');
-  //     expect(test.totalVertices).to.equal(1);
-  //
-  //     test.removeVertex('hello');
-  //     expect(test.vertices['hello']).to.equal(undefined);
-  //   })
-  // })
-  //
-  // describe('graph addEdge method', function(){
-  //   it('should create an edge between two vertices', function(){
-  //     const test = new Graph();
-  //
-  //     test.addVertex('hello');
-  //     test.addVertex('goodbye');
-  //
-  //     test.addEdge('hello', 'goodbye');
-  //
-  //     expect(test.vertices['hello'].edges['goodbye']).to.equal('goodbye');
-  //     expect(test.vertices['goodbye'].edges['hello']).to.equal('hello');
-  //   })
-  //
-  //   it('should increase the edge counter', function(){
-  //     const test = new Graph();
-  //
-  //     test.addVertex('hello');
-  //     test.addVertex('goodbye');
-  //
-  //     test.addEdge('hello', 'goodbye');
-  //     expect(test.totalEdges).to.equal(1);
-  //   })
-  // })
-  //
-  // describe('graph removeEdge method', function(){
-  //   it('should remove an edge between two vertices', function(){
-  //     const test = new Graph();
-  //
-  //     test.addVertex('hello');
-  //     test.addVertex('goodbye');
-  //
-  //     test.addEdge('hello', 'goodbye');
-  //
-  //     expect(test.vertices['hello'].edges['goodbye']).to.equal('goodbye');
-  //     expect(test.vertices['goodbye'].edges['hello']).to.equal('hello');
-  //
-  //     test.removeEdge('hello', 'goodbye');
-  //     expect(test.vertices['hello'].edges['goodbye']).to.equal(undefined);
-  //     expect(test.vertices['goodbye'].edges['hello']).to.equal(undefined);
-  //     expect(test.totalEdges).to.equal(0);
-  //   })
-  //   it('should not decrease the totalEdges counter when an edge does not exist between two vertices', function(){
-  //     const test = new Graph();
-  //
-  //     test.addVertex('hello');
-  //     test.addVertex('goodbye');
-  //     test.addVertex('blah');
-  //
-  //     test.addEdge('hello', 'goodbye');
-  //
-  //     expect(test.vertices['hello'].edges['goodbye']).to.equal('goodbye');
-  //     expect(test.vertices['goodbye'].edges['hello']).to.equal('hello');
-  //
-  //     test.removeEdge('hello', 'blah');
-  //     expect(test.totalEdges).to.equal(1);
-  //   })
-  // })
-  //
-  // describe('graph findNeighbors method', function(){
-  //   it("should return an array of the neighbors' IDs", function(){
-  //     const test = new Graph();
-  //
-  //     test.addVertex('hello');
-  //     test.addVertex('goodbye');
-  //     test.addVertex('blah');
-  //     test.addVertex('haha');
-  //
-  //     test.addEdge('hello', 'goodbye');
-  //     test.addEdge('hello', 'blah');
-  //
-  //     expect(test.vertices['hello'].edges['goodbye']).to.equal('goodbye');
-  //     expect(test.vertices['goodbye'].edges['hello']).to.equal('hello');
-  //     expect(test.vertices['hello'].edges['blah']).to.equal('blah');
-  //     expect(test.vertices['blah'].edges['hello']).to.equal('hello');
-  //
-  //     const result = test.findNeighbors('hello');
-  //     expect(result).to.include('goodbye');
-  //     expect(result).to.include('blah');
-  //   })
-  //
-  //   it("should return an empty array when there's no edges", function(){
-  //     const test = new Graph();
-  //
-  //     test.addVertex('hello');
-  //     test.addVertex('goodbye');
-  //     test.addVertex('blah');
-  //     test.addVertex('haha');
-  //
-  //     test.addEdge('hello', 'goodbye');
-  //     test.addEdge('hello', 'blah');
-  //
-  //     const result = test.findNeighbors('haha');
-  //     expect(result.length).to.equal(0);
-  //   })
-  // })
-  //
-  // describe('graph forEachNode method', function(){
-  //   it('should perform the callback on all of the nodes', function(){
-  //     // insert test here
-  //   })
-  // })
+
+    it('should add two vertices', function(){
+      const graph = new test.Graph();
+      expect(graph.vertices['mofthecross']).to.equal(undefined);
+
+      graph.addVertex('mofthecross');
+      expect(graph.vertices['mofthecross'].value).to.equal('mofthecross');
+      expect(graph.totalVertices).to.equal(1);
+
+      graph.addVertex('good');
+      expect(graph.vertices['good'].value).to.equal('good');
+      expect(graph.totalVertices).to.equal(2);
+    });
+
+    it('should not increase the totalVertices count when the vertex already exists', function(){
+      const graph = new test.Graph();
+      expect(graph.vertices['mofthecross']).to.equal(undefined);
+
+      graph.addVertex('mofthecross');
+      expect(graph.vertices['mofthecross'].value).to.equal('mofthecross');
+      expect(graph.totalVertices).to.equal(1);
+
+      graph.addVertex('mofthecross');
+      expect(graph.totalVertices).to.equal(1);
+    });
+  });
+
 })
