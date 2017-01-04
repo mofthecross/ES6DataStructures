ee83150f910c0b051c75b6f10a35b22de546ea37
mofthecross <mofthecross@gmail.com>
2017-01-03 19:07:52 -0800

    Implement addEdge method on a graph

--- main/graph.js
+++ main/graph.js
@@ -24,4 +24,17 @@ class Graph {
       console.log('id does not exist');
     }
   }
+  addEdge(id) {
+    if (this.vertices[id1] !== undefined && this.vertices[id2] !== undefined) {
+      if(this.vertices[id1].edges[id2] === undefined && this.vertices[id2].edges[id1] === undefined) {
+        this.vertices[id1].edges[id2] = id2;
+        this.vertices[id2].edges[id1] = id1;
+        this.totalEdges++;
+      } else {
+        console.log('edge already exists!');
+      }
+    } else {
+      console.log('either vertex or both do not exists!');
+    }
+  }
 }
