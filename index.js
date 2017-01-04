fd227db78b8c92cc07eee31f7a4fd1782d0ed080
mofthecross <mofthecross@gmail.com>
2017-01-03 23:25:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 module.exports.DoublyLinkedList = require('./main/doublyLinkedList.js').DoublyLinkedList;
 module.exports.Graph = require('./main/graph.js').Graph;
+module.exports.Stack = require('./main/stack.js').Stack;
+module.exports.Queue = require('./main/stack.js').Queue;
