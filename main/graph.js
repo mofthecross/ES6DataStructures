3e8312e565ccee3f9e1f97f3d9403f6e57caada3
mofthecross <mofthecross@gmail.com>
2017-01-03 23:22:54 -0800

    Implement breadthFirstSearch and depthFirstSearch methods on a graph

--- main/graph.js
+++ main/graph.js
@@ -11,12 +11,14 @@ class Graph {
     this.totalVertices = 0;
     this.totalEdges = 0;
   }
+
   addVertex(id) {
     if (this.vertices[id] === undefined) {
       this.vertices[id] = new Vertex(id);
       this.totalVertices++;
     }
   }
+
   getVertex(id) {
     if (this.vertices[id] !== undefined) {
       return this.vertices[id];
@@ -24,6 +26,7 @@ class Graph {
       console.log('id does not exist');
     }
   }
+
   addEdge(id1, id2) {
     if (this.vertices[id1] !== undefined && this.vertices[id2] !== undefined) {
       if(this.vertices[id1].edges[id2] === undefined && this.vertices[id2].edges[id1] === undefined) {
@@ -37,6 +40,7 @@ class Graph {
       console.log('either vertex or both do not exists!');
     }
   }
+
   removeEdge(id1, id2) {
     if (this.vertices[id1] !== undefined && this.vertices[id2] !== undefined) {
       if(this.vertices[id1].edges[id2] !== undefined && this.vertices[id2].edges[id1] !== undefined) {
@@ -50,6 +54,7 @@ class Graph {
       console.log('either vertex or both do not exists!');
     }
   }
+
   removeVertex(id) {
     if (this.vertices[id] !== undefined) {
       let toDelete = this.vertices[id];
@@ -74,11 +79,60 @@ class Graph {
       console.log("ID of vertex does not exit");
     }
   }
+
   forEachNode(cb){
     for(vertexId in this.vertices) {
       cb(this.vertices[vertexId]);
     }
   }
+}
+
+/** traversal methods **/
+const Queue = require('./queue.js').Queue;
+const Stack = require('./stack.js').Stack;
+
+Graph.prototype.breadthFirstSearch = function(id) {
+  const result = [],
+        queue = new Queue(),
+        travelled = {};
+
+  queue.enqueue(this.vertices[id]);
+  travelled[id] = true;
+
+  while(queue.size() > 0) {
+    let currentNode = queue.dequeue();
+
+    for (let edge in currentNode.edges) {
+      if (!travelled[edge]) {
+        queue.enqueue(this.vertices[edge]);
+        travelled[edge] = true;
+      }
+    }
+    result.push(currentNode.value);
+  }
+  return result;
+}
+
+
+Graph.prototype.depthFirstSeach = function(id) {
+  const result = [],
+        stack = new Stack(),
+        travelled = {};
+
+  stack.push(this.vertices[id]);
+  travelled[id] = true;
+
+  while(stack.length > 0) {
+    let currentNode = stack.pop();
+    for (let edge in currentNode.edges) {
+      if(!travelled[edge]) {
+        stack.push(this.vertices[edge]);
+        travelled[edge] = true;
+      }
+    }
+    result.push(currentNode.value);
+  }
+  return result;
 }
 
 module.exports.Vertex = Vertex;
