0d6af6f2c21465169d718418d7aeb2f9f9eb787c
mofthecross <mofthecross@gmail.com>
2017-01-03 21:10:14 -0800

    Implement pop method on Stack

--- main/stack.js
+++ main/stack.js
@@ -4,11 +4,20 @@ class Stack {
     this._length = 0;
   }
   push(data) {
-    this._storage[this._length] = data;
+    this._storage[this._length + 1] = data;
     this._length++;
+  }
+  pop() {
+    if (this._length > 0) {
+      const deleted = this._storage[this._length];
+      delete this._storage[this._length];
+      this._length--;
+      return deleted;
+    }
   }
 }
 
 let newStack = new Stack();
 newStack.push('8');
+newStack.pop()
 console.log(newStack);
