0e88cb2d42016b0bad1e3f2d5afa78336cb9b512
mofthecross <mofthecross@gmail.com>
2017-06-06 16:57:54 -0700

    Include BST class in Index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports.DoublyLinkedList = require('./main/doublyLinkedList.js').DoublyLinkedList;
 module.exports.Graph = require('./main/graph.js').Graph;
 module.exports.Stack = require('./main/stack.js').Stack;
-module.exports.Queue = require('./main/stack.js').Queue;
+module.exports.Queue = require('./main/queue.js').Queue;
+module.exports.BinarySearchTree = require('./main/binarySearchTree.js').BinarySearchTree;
