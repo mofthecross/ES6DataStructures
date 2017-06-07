53ca9cc76165e6aaed3a44b6ac2301f1ce226fb3
mofthecross <mofthecross@gmail.com>
2017-06-07 16:51:22 -0700

    Add utils file and implement stringify function in order to see deeply nested objects

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ module.exports.Stack = require('./main/stack.js').Stack;
 module.exports.Queue = require('./main/queue.js').Queue;
 module.exports.BinarySearchTree = require('./main/binarySearchTree.js').BinarySearchTree;
 module.exports.BinaryTreeNode = require('./main/binaryTreeNode.js').BinaryTreeNode;
+
+// utils
+module.exports.stringify = require('./ultils.js').stringify;
---
+++ utils.js
@@ -0,0 +1,53 @@
+/*
+stringify is a function that allows you to see deeply nested objects.
+stringify returns a string with given indentation/spaces for each line.
+
+INPUT:
+  const newBT = new BinaryTreeNode(5);
+  newBT.leftChild = new BinaryTreeNode(2);
+  newBT.rightChild = new BinaryTreeNode(7);
+  newBT.rightChild.leftChild = new BinaryTreeNode(4);
+  newBT.rightChild.rightChild = new BinaryTreeNode(9);
+
+  stringify(newBT, 9);
+
+OUTPUT:
+  {
+           "value": 5,
+           "leftChild": {
+                    "value": 2,
+                    "leftChild": null,
+                    "rightChild": null
+           },
+           "rightChild": {
+                    "value": 7,
+                    "leftChild": {
+                             "value": 4,
+                             "leftChild": null,
+                             "rightChild": null
+                    },
+                    "rightChild": {
+                             "value": 9,
+                             "leftChild": null,
+                             "rightChild": null
+                    }
+           }
+  }
+
+*/
+
+const stringify = (obj, space) => {
+ const seen =[];
+
+  return JSON.stringify(obj, function(key, val) {
+     if (val != null && typeof val == "object") {
+          if (seen.indexOf(val) >= 0) {
+              return;
+          }
+          seen.push(val);
+      }
+      return val;
+  }, space);
+}
+
+module.exports = stringify
