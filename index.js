9e3194c0277ef2f88d6140c71139d5f0f104255b
mofthecross <mofthecross@gmail.com>
2017-06-07 16:16:09 -0700

    Add BinaryTreeNode class to Index

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ module.exports.Graph = require('./main/graph.js').Graph;
 module.exports.Stack = require('./main/stack.js').Stack;
 module.exports.Queue = require('./main/queue.js').Queue;
 module.exports.BinarySearchTree = require('./main/binarySearchTree.js').BinarySearchTree;
+module.exports.BinaryTreeNode = require('./main/binaryTreeNode.js').BinaryTreeNode;
