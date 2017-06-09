01174fdf5ef71321bfe933b6770dc3079de35c88
mofthecross <mofthecross@gmail.com>
2017-06-09 16:51:15 -0700

    Implement swap method on MinHeap

--- main/heap.js
+++ main/heap.js
@@ -7,6 +7,13 @@ class MinHeap {
     return this.storage.length;
   }
 
+  //swap two items in the 'tree';
+  swap(index1, index2) {
+    const temp = this.storage[index1];
+    this.storage[index1] = this.storage[index2];
+    this.storage[index2] = temp;
+  }
+
 }
 
 module.exports.MinHeap = MinHeap;
