c87b4106606f33e9d7aa16b17e2b3fb64114d0f6
mofthecross <mofthecross@gmail.com>
2017-06-09 18:10:58 -0700

    update index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-module.exports.DoublyLinkedList = require('./main/doublyLinkedList.js').DoublyLinkedList;
-module.exports.Graph = require('./main/graph.js').Graph;
-module.exports.Stack = require('./main/stack.js').Stack;
-module.exports.Queue = require('./main/queue.js').Queue;
-module.exports.BinarySearchTree = require('./main/binarySearchTree.js').BinarySearchTree;
-module.exports.BinaryTreeNode = require('./main/binaryTreeNode.js').BinaryTreeNode;
+module.exports.DoublyLinkedList = require('./dataStructures/doublyLinkedList.js').DoublyLinkedList;
+module.exports.Graph = require('./dataStructures/graph.js').Graph;
+module.exports.Stack = require('./dataStructures/stack.js').Stack;
+module.exports.Queue = require('./dataStructures/queue.js').Queue;
+module.exports.BinarySearchTree = require('./dataStructures/binarySearchTree.js').BinarySearchTree;
+module.exports.BinaryTreeNode = require('./dataStructures/binaryTreeNode.js').BinaryTreeNode;
 
+module.exports.mergeSort = require('./sortingAlgorithms/mergeSort.js').mergeSort;
+module.exports.quickSort = require('./sortingAlgorithms/quickSort.js').quickSort;
 // utils
 module.exports.stringify = require('./utils.js').stringify;
