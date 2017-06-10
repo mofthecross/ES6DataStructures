94be15dd0cb820e0e58972d1481c95b8fcbd36c1
mofthecross <mofthecross@gmail.com>
2017-06-09 17:27:03 -0700

    Finish test and refactor code to use arrow function

--- tests/minHeapTest.js
+++ tests/minHeapTest.js
@@ -1,18 +1,18 @@
 const expect = require('chai').expect;
 const MinHeap = require('../main/heap.js').MinHeap;
 
-describe('min heap tree class ', function() {
+describe('min heap tree class ', () =>  {
 
-  describe('MinHeap properties', function() {
-    it('should have properties storage', function() {
+  describe('MinHeap properties', () =>  {
+    it('should have properties storage', () =>  {
       const test = new MinHeap();
       expect(test).to.have.property('storage');
       expect(test.storage.length).to.equal(0);
     });
   });
 
-  describe('MinHeap methods existence', function(){
-    it('should have methods insert, swap, bubbleUp, bubbleDown, peak, removePeak, size, getParentIndex, getChildrenIndeces, getMasterChildIndex', function(){
+  describe('MinHeap methods existence', () => {
+    it('should have methods insert, swap, bubbleUp, bubbleDown, peak, removePeak, size, getParentIndex, getChildrenIndeces, getMasterChildIndex', () => {
         const test = new MinHeap();
         expect(test).to.respondTo('insert');
         expect(test).to.respondTo('swap');
@@ -29,8 +29,8 @@ describe('min heap tree class ', function() {
   });
 
 
-  describe('MinHeap insert method', function(){
-    it('should be able to insert a single value and return peak value ', function(){
+  describe('MinHeap insert method', () => {
+    it('should be able to insert a single value and return peak value ', () => {
       const test = new MinHeap();
 
       expect(test.size()).to.equal(0);
@@ -40,7 +40,7 @@ describe('min heap tree class ', function() {
       expect(test.size()).to.equal(1);
     });
 
-    it('should be able to insert a second value', function(){
+    it('should be able to insert a second value', () => {
       const test = new MinHeap();
 
       expect(test.size()).to.equal(0);
@@ -52,6 +52,31 @@ describe('min heap tree class ', function() {
       test.insert(10);
       expect(test.peak()).to.equal(5);
       expect(test.size()).to.equal(2);
+    });
+  });
+
+  describe('MinHeap removePeak method', () => {
+    it('should delete a value and reogarnize elements according to min heap rules', () => {
+/*
+              0
+             /  \
+           1      7
+         /  \    /  \
+        2    5  8    10
+      /
+      9
+      index:  0 1 2 3 4 5  6 7
+      array: [0,1,7,2,5,8,10,9];
+*/
+      const test = new MinHeap();
+      [0,1,7,2,5,8,10,9].map(function(value) {
+        test.insert(value);
+      });
+
+      expect(test.peak() === 0).to.equal(true);
+      test.removePeak();
+      expect(test.peak()).to.equal(1);
+      test.insert(0);
     });
   });
 
