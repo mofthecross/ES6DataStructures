adfca81e1e52e6e87e453afe735ca9f99bc5206c
mofthecross <mofthecross@gmail.com>
2017-06-09 19:38:56 -0700

    Update index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports.Graph = require('./dataStructures/graph.js').Graph;
 module.exports.Stack = require('./dataStructures/stack.js').Stack;
 module.exports.Queue = require('./dataStructures/queue.js').Queue;
 module.exports.BinarySearchTree = require('./dataStructures/binarySearchTree.js').BinarySearchTree;
-module.exports.BinaryTreeNode = require('./dataStructures/binaryTreeNode.js').BinaryTreeNode;
+module.exports.BinaryTreeNode = require('./dataStructures/binaryTreeNode.js');
 
 module.exports.mergeSort = require('./sortingAlgorithms/mergeSort.js').mergeSort;
 module.exports.quickSort = require('./sortingAlgorithms/quickSort.js').quickSort;
