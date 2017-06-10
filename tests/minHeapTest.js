012777bba06d001d88e721ad47727b80232d79fb
mofthecross <mofthecross@gmail.com>
2017-06-09 17:07:52 -0700

    Write test for methods existence on minHeap class

--- tests/minHeapTest.js
+++ tests/minHeapTest.js
@@ -10,4 +10,21 @@ describe('min heap tree class ', function() {
       expect(test.storage.length).to.equal(0);
     });
   });
+
+  describe('MinHeap methods existence', function(){
+    it('should have methods insert, swap, bubbleUp, bubbleDown, peak, removePeak, size, getParentIndex, getChildrenIndeces, getMasterChildIndex', function(){
+        const test = new MinHeap();
+        expect(test).to.respondTo('insert');
+        expect(test).to.respondTo('swap');
+        expect(test).to.respondTo('bubbleUp');
+        expect(test).to.respondTo('bubbleDown');
+        expect(test).to.respondTo('peak');
+        expect(test).to.respondTo('removePeak');
+        expect(test).to.respondTo('size');
+        expect(test).to.respondTo('getParentIndex');
+        expect(test).to.respondTo('getChildrenIndeces');
+        expect(test).to.respondTo('getMasterChildIndex');
+        expect(test).to.respondTo('size');
+    });
+  });
 });
