86594e4c0e362660de538ffde8f5634cdd4c5185
mofthecross <mofthecross@gmail.com>
2017-06-17 14:41:23 -0700

    Implement hash method;

--- dataStructures/hashTable.js
+++ dataStructures/hashTable.js
@@ -1,7 +1,19 @@
 class HashTable {
   constructor() {
-    this.storage = [];
+    // The max number of buckets that your storage can contain.     *
+    // "buckets" is initialized at 8 buckets.
     this.buckets = 8;
     this.size = 0;
+    this.storage = [];
+  }
+
+  hash (str, buckets) {
+    let hash = 5381;
+    for (let i = 0; i < str.length; i++) {
+      let char = str.charCodeAt(i);
+      // hash * 33 + c
+      hash = ((hash << 5) + hash) + char;
+    }
+    return hash % buckets;
   }
 }
